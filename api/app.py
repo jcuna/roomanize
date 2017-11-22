585064fc5d33425807bf2d412223ff093c57e572
Jon Garcia <garciajon@me.com>
2017-11-21 22:46:21 -0500

    nginx to serve both api and static

--- api/app.py
+++ api/app.py
@@ -19,5 +19,5 @@ class Test(Resource):
 
 api.add_resource(Test, "/")
 
-if (__name__ == '__main__'):
-    app.run(host='0.0.0.0', port=80, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ api/wsgi.py
@@ -0,0 +1,6 @@
+from app import app
+
+print(__name__)
+
+if __name__ == '__main__':
+    app.run()
