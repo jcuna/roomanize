df9f3c711fbb1b23a85d8819dff26a29128452de
Jon Garcia <garciajon@me.com>
2019-05-31 10:47:43 -0400

    fixing prod socketio issue

--- api/wsgi.py
+++ api/wsgi.py
@@ -2,7 +2,7 @@ from app import init_app
 from flask_socketio import SocketIO
 
 app = init_app()
+socketio = SocketIO(app)
 
 if __name__ == '__main__':
-    socketio = SocketIO(app)
     socketio.run(app)
